c325ff26867b9048fa367e656db1e2de9902ca22
ayaamghi <ayaamghi@gmail.com>
2023-05-21 13:10:46 -0400

    ONly confirmed portfolios, UI same as Plain

--- src/Components/ports.js
+++ src/Components/ports.js
@@ -29,23 +29,6 @@ new Portfolio(
     1
   ),
 new Portfolio(
-    'https://docs.google.com/presentation/d/1VmaWUsDWUPmsJHuX0or0l4PHz9xuelqKTNNAwc2PvB4/edit#slide=id.g200d08e9569_9_0',
-    2022,
-    'States',
-    'Think',
-    'CircuitRunners Robotics',
-    11347,
-    1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1ikjJQyTCAZtsihuesrip-4nT3HAddKHA/view',
-    2022,
-    'States',
-    'None',
-    'ROBOKEY',
-    20963
-  ),
-new Portfolio(
     'https://drive.google.com/file/d/1fIZymkyWdKABecqoFYoQRo9KIoIHjp5c/view',
     2022,
     'Regionals',
@@ -60,102 +43,6 @@ new Portfolio(
     'None',
     'Everglow',
     19000
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1Nj132z4uXUeY2nhlw_mW4_vJTLAul4shcWDfc7lFBa0/view',
-    2022,
-    'States',
-    'Control',
-    'Metrobotics',
-    14212,
-    1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1WXklCHxZ0sBJ85_xzLZfk2lXfS1p67mQ/view?usp=drivesdk',
-    2021,
-    'States',
-    'None',
-    'Tacobotics',
-    18369
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1UIDdnHA5j_3jQmccV3sCNq3JVIJ85DbT/view',
-    2022,
-    'States',
-    'Design',
-    'Steel Eels',
-    18317,
-    1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/12obKljuXU87bNrqK39z1a_4NLCeM53qe/view',
-    2021,
-    'States',
-    'None',
-    'Thunderbolts in Disguise',
-    18140
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1smRuG-DgZ_mCt_2eG_G14JpAcm8YeZmM/view',
-    2022,
-    'Control',
-    'Regionals',
-    'Protocol 3',
-    17520,
-    1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1YS8bXsOQ30YTk_3abN4nD0jEeXFq29yB/view',
-    2022,
-    'Worlds',
-    'None',
-    'Newt.exe',
-    17080
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1Ufg2t8qW-vhH7CrsiNtTHavHCn23bklf/view',
-    2022,
-    'States',
-    'Think',
-    'Bots in Black',
-    16633,
-    1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/18mUIdXKA-zyXXsyzHNCWVZU4qya9arqJ/view',
-    2022,
-    'Regionals',
-    'Think',
-    'Java Like It\'s Hot',
-    16553,
-    1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1wSkkZyvcX5iaXPFEIc6KtKT5_p1817Gi/view',
-    2020,
-    'Regionals',
-    'Think',
-    'Juice',
-    16236,
-    1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1nT86VotoR_wEH_jCuJGtm9HjX24lxdxf/view',
-    2022,
-    'States',
-    'Design',
-    'Circuit Breakers',
-    16232,
-    3
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1Ufg2t8qW-vhH7CrsiNtTHavHCn23bklf/view',
-    2022,
-    'Worlds',
-    'Think',
-    'Team Without a Cool Acronym',
-    16091,
-    1
   ),
 new Portfolio(
     'https://drive.google.com/file/d/1WfDzCNLlDfKlr7MRGtF9DvdyR4e8Ad0R/view?usp=drivesdk',
@@ -203,42 +90,6 @@ new Portfolio(
     1
   ),
 new Portfolio(
-    'https://drive.google.com/file/d/1CKFj_G6W__o3WMLjuCc3U7GH1GBll5L4/view?usp=drivesdk',
-    2021,
-    'States',
-    'Inspire',
-    'CircuitRunners Robotics - Surge',
-    1002,
-    1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1NfZqoh4ZaUnfbtFu7jICubrkeH1KXH9V/view?usp=drivesdk',
-    2021,
-    'Worlds',
-    'Innovate',
-    'CircuitRunners Robotics - Surge',
-    1002,
-    3
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1QjhpWx3fG228G8vXc7JVTkenESFqrBM_/view?usp=drivesdk',
-    2020,
-    'States',
-    'Inspire',
-    'Astromechs',
-    3409,
-    2
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1E2-qA8EumXqHmvOagwKHN0oPJw32sYtQ/view?usp=drivesdk',
-    2021,
-    'Inspire',
-    'States',
-    'Astromechs',
-    3409,
-    1
-  ),
-new Portfolio(
     'https://drive.google.com/file/d/16Iw5cWNYYq_oDRYcyeHsDcysZiWrKTAm/view?usp=drivesdk',
     2021,
     'Regionals',
@@ -246,16 +97,6 @@ new Portfolio(
     'Nonstandard Deviation',
     4052,
     2
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1BSp6t9zgZXDT4Q5YSxS9vWrNUvw9pst0/view?usp=drivesdk',
-    2022,
-    'Regionals',
-    'Innovate',
-    'Team Wolves',
-    5189,
-    1
-   
   ),
 new Portfolio(
     'https://drive.google.com/file/d/15vmlLtDykHxyK1ABqwW7fiPA7K2GL5iq/view?usp=drivesdk',
@@ -274,24 +115,6 @@ new Portfolio(
     'Back To The Drawing Board',
     5795,
     1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1QaCnySDooEWixa1ZKNNw4M59Oy1PsRb0/view?usp=drivesdkg',
-    2021,
-    'Regionals',
-    'Connect',
-    'Tungsteel',
-    5881,
-    1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1XnO7WAnMTDCPVIUEPYgmQ6XkMHraiMKt/view?usp=drivesdk',
-    2020,
-    'States',
-    'Connect',
-    'e-lemon-ators',
-    5890,
-    2
   ),
 new Portfolio(
     'https://drive.google.com/file/d/1kvaoFU1gRfysOZINSV9V737n3l0kHktz/view?usp=drivesdk',
@@ -313,152 +136,12 @@ new Portfolio(
     
   ),
 new Portfolio(
-    'https://drive.google.com/file/d/1QnTMEDBAyzFrY80HPUmdBKBNCUgWrYrz/view?usp=drivesdk',
-    2021,
-    'Regionals',
-    'Motivate',
-    'The Indecision Matrices',
-    6527,
-    1
-  ),
-new Portfolio(
     'https://drive.google.com/file/d/17ts0AyVxj_DJ5dxF3P0RuzLfeDxJVSYh/view?usp=drivesdk',
     2021,
     'Worlds',
     'Nothing',
     'Team Without a Cool Acronym',
     16091
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1icJGESZWLSvKITaV2EvRj8JfqZVOSTfH/view?usp=drivesdk',
-    2021,
-    'States',
-    'Nothing',
-    'BrickBot',
-    15996
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1UpIHjDn8yb-teKf5t0fvdo2IxHQVsSK4/view?usp=drivesdk',
-    2020,
-    'States',
-    'Design',
-    'Weber RamBots:',
-    14943,
-    1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1_k9yoCJK8hDffg3SlhtRDH7FPyq1gQc3/view?usp=drivesdk',
-    2021,
-    'Regionals',
-    'Control',
-    'Alpacas',
-    14535,
-    1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1oKppTjHvnINdLt5Z4xc0B3xTPDLffkKR/view?usp=drivesdk',
-    2021,
-    'Worlds',
-    'Nothing',
-    'Roaring Robotics',
-    14436
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1SRmvt0MWoay9grHCfHhZXta3N41MeBm8/view?usp=drivesdk',
-    2022,
-    'States',
-    'Motivate',
-    'Quantum Robotics',
-    14270,
-    1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/13LtUCehKWFkSinvkZMYPeBOKsTd8WUtM/view?usp=drivesdk',
-    2021,
-    'States',
-    'Judges',
-    'The Java Jokers',
-    13828,
-    1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1YFm8YJ3S3PmrFkhCnbYnqCOmd1ikm57l/view?usp=drivesdk',
-    2022,
-    'Regionals',
-    'Nothing',
-    'Code Blue',
-    13193
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1Tfo91rcNJ80USFZrTdjlvEt3NGZRxbAq/view?usp=drivesdk',
-    2020,
-    'States',
-    'Innovate',
-    'Free Wifi',
-    13181,
-    3
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/13tTEnPS-lA7dqWkJsoAAqKawqDtzI4pL/view?usp=drivesdk',
-    2022,
-    'States',
-    'Inspire',
-    'Berserk Robotics',
-    13053,
-    1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1FU9wWpuAnSYoQTdRZ1xrGqVxTLwS0DL9/view?usp=drivesdk',
-    2021,
-    'Worlds',
-    'Design',
-    'Kuriosity Robotics',
-    12635,
-    1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/18UfKrBYk_UaXE0KdQ_unn-xgC_ppD30S/view?usp=drivesdk',
-    2020,
-    'Regionals',
-    'Inspire',
-    'Space Turtles',
-    12547,
-    1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1onpbkosXNcnfFxWATdUtVkOSmejNngTk/view?usp=drivesdk',
-    2022,
-    'Regionals',
-    'Inspire',
-    'Not Not Nerds',
-    12051,
-    1
-  ),
-new Portfolio(
-    'https://docs.google.com/presentation/d/1MCaAQW8Qc8mXM3eL69A3MVt0Dut9Osauz9DXAai9LFk/edit?usp=sharing',
-    2022,
-    'Worlds',
-    'Nothing',
-    'Ohm Raiders',
-    11468
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1_MwV5Mk57-Sg8F-VN_II1jexmwpsDXZH/view?usp=drivesdk',
-    2021,
-    'States',
-    'Control',
-    'Cyborg Cats',
-    11231,
-    3
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1hTnRMOVD97SLEzqGPZN61TpLzGgaph6w/view?usp=drivesdk',
-    2021,
-    'States',
-    'Motivate',
-    'Atomic Gears',
-    10641,
-    1
   ),
 new Portfolio(
     'https://drive.google.com/file/d/1h5iFMbuP40OicQ2uELjTRor8rGCf2HeW/view?usp=drivesdk',
@@ -478,32 +161,6 @@ new Portfolio(
     10195
   ),
 new Portfolio(
-    'https://drive.google.com/file/d/1-r9mZwOO1HIt6Ndc0k2N-uYRQX2UHigL/view?usp=drivesdk',
-    2021,
-    'Worlds',
-    'Nothing',
-    'F.R.O.G Frog Robots of Germany',
-    10183
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1OONW639qH39dJpNdJikqc_LUufwx2WuU/view?usp=drivesdk',
-    2022,
-    'States',
-    'Control',
-    'Tech Turtles',
-    10179,
-    2
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1qDv7er-N_gmH_hDnE_8GoNMUKtQ1RviE/view?usp=drivesdk',
-    2021,
-    'Regionals',
-    'Connect',
-    'T.H.O.R.',
-    9974,
-    3
-  ),
-new Portfolio(
     'https://drive.google.com/file/d/1JKHxy37gIhD0CQL5paDs8IX7Z7r6RIlP/view?usp=drivesdk',
     2020,
     'States',
@@ -511,32 +168,6 @@ new Portfolio(
     'T.H.O.R.',
     9974,
     3
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1cbzWjdzdszGl3volFMCS0eo-VG9YObqR/view?usp=drivesdk',
-    2020,
-    'States',
-    'Innovate',
-    'Highly Combustible',
-    9548,
-    2
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1fiwITpKo_KmZZfB2SmLo-TIDy-YwZy2P/view?usp=drivesdk',
-    2020,
-    'Regionals',
-    'Control',
-    'Rogue Resistance',
-    9527,
-    2
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/18YoysmvTL87VVS7EMMoqGdQg_1s4uu53/view?usp=drivesdk',
-    2021,
-    'States',
-    'Nothing',
-    'If it Ain\'t Broke',
-    9219
   ),
 new Portfolio(
     'https://drive.google.com/file/d/1pgTptjwJnBg0MiexpHs5V5IcSn2bZjgt/view?usp=drivesdk',
@@ -553,41 +184,6 @@ new Portfolio(
     'Design',
     'Pi Rho Eagles',
     8300,
-    3
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1NaFzN00Jxy1poZVtFswhwWbulXzWF-Bx/view?usp=drivesdk',
-    2021,
-    'Regionals',
-    'Innovate',
-    'Robogoats',
-    8161,
-    3
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1rBy0Bx12q6ene0F61kw7RBZFXe84VRR_/view?usp=drivesdk',
-    2020,
-    'States',
-    'Nothing',
-    'Robogoats',
-    8161
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1ZFqwxkydAWv7es3UOCJgZYlPTE3Con8Z/view?usp=drivesdk',
-    2021,
-    'Regionals',
-    'Motivate',
-    'Invicta',
-    8110,
-    1
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1Wp1bVx_sul5kFPuTSWvAokERMF-8pbN4/view?usp=drivesdk',
-    2020,
-    'States',
-    'Inspire',
-    'Browncoats',
-    7842,
     3
   ),
 new Portfolio(
@@ -608,22 +204,6 @@ new Portfolio(
     2
   ),
 new Portfolio(
-    'https://drive.google.com/file/d/1iPQ5MhfeQa-dkkowfa5nC7ZGRnw08j5k/view?usp=drivesdk',
-    2021,
-    'States',
-    'Nothing',
-    'F.R.E.N.C.H. F.R.I.E.S.',
-    7341
-  ),
-new Portfolio(
-    'https://drive.google.com/file/d/1CMDdADSZt5fZOcWfyOfkUnUQpPqDWAno/view?usp=drivesdk',
-    2021,
-    'Worlds',
-    'Nothing',
-    'Cobalt Colts',
-    6547
-),
-new Portfolio(
     'https://drive.google.com/file/d/1pFmdzmupxY_W05NrS9Yae1mCfmxHfmMo/view?usp=drivesdk',
     2021,
     'States',
@@ -632,5 +212,24 @@ new Portfolio(
     7444,
     2
   ),
-  
+  new Portfolio(
+    'https://drive.google.com/file/d/1-r9mZwOO1HIt6Ndc0k2N-uYRQX2UHigL/view?usp=drivesdk',
+    2021,
+    'Worlds',
+    'Nothing',
+    'F.R.O.G Frog Robots of Germany',
+    10183
+  ),
+  new Portfolio(
+    'https://drive.google.com/file/d/1fiwITpKo_KmZZfB2SmLo-TIDy-YwZy2P/view?usp=drivesdk',
+    2020,
+    'Regionals',
+    'Control',
+    'Rogue Resistance',
+    9527,
+    2
+  ),
+
 ];
+
+console.log(Ports.length)
