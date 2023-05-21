e7f64ab5c58d802b87f7db043bfa13ba4311b827
ayaamghi <ayaamghi@gmail.com>
2023-05-21 12:50:13 -0400

    added ownership

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -18,6 +18,8 @@ const Team = () => {
                 <p style = {{}}className="text-3xl font-bold col-auto text-black ">FTC Portfolio Utility- Presented by Team 10195 Night Owls </p>     
                 <a className = ' text-black px-4' target="_blank" href = "https://ecgrobotics.org/ftc10195/">About the Team</a>       
                 <a className = 'text-black' target="_blank" href = "https://docs.google.com/forms/d/e/1FAIpQLSdEO4ifptS7LqIjBBJTRyEO3BSJ6wb-KkQPREObv2NO3X1GXg/viewform?embedded=true ">Submit Portfolio</a>       
+                <a className = 'text-black px-4' target="_blank" href = "">About the Site</a>       
+
             </div>
             
         );
