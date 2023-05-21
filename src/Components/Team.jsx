9c5cc83c025f3bccdec4cd7473d4dda02233a6f2
ayaamghi <ayaamghi@gmail.com>
2023-05-21 13:05:49 -0400

    more stuff

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -14,11 +14,11 @@ const Team = () => {
     }
     
     return(
-            <div className ="bg-orange-500  border-white ">
-                <p style = {{}}className="text-3xl font-bold col-auto text-black ">FTC Portfolio Utility- Presented by Team 10195 Night Owls </p>     
-                <a className = ' text-black px-4' target="_blank" href = "https://ecgrobotics.org/ftc10195/">About the Team</a>       
-                <a className = 'text-black' target="_blank" href = "https://docs.google.com/forms/d/e/1FAIpQLSdEO4ifptS7LqIjBBJTRyEO3BSJ6wb-KkQPREObv2NO3X1GXg/viewform?embedded=true ">Submit Portfolio</a>       
-                <a className = 'text-black px-4' target="_blank" href = "">About the Site</a>       
+            <div className ="bg-blue-600  border-white ">
+                <p style = {{}}className="text-3xl font-bold col-auto text-white ">FTC Portfolio Utility- Presented by Team 10195 Night Owls </p>     
+                <a className = ' text-white px-4' target="_blank" href = "https://ecgrobotics.org/ftc10195/">About the Team</a>       
+                <a className = 'text-white' target="_blank" href = "https://docs.google.com/forms/d/e/1FAIpQLSdEO4ifptS7LqIjBBJTRyEO3BSJ6wb-KkQPREObv2NO3X1GXg/viewform?embedded=true ">Submit Portfolio</a>       
+                <a className = 'text-white px-4' target="_blank" href = "">About the Site</a>       
 
             </div>
             
